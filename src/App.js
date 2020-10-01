8433caa09f2cd505812092683446a468c1198889
Stephen Peck <speck1990@gmail.com>
2020-09-30 22:31:25 -0500

    Add AuthState to app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 
+import AuthState from "./context/auth/AuthState";
+
 const App = () => {
 	return (
-		<Router>
-			<div>
-				<h1>Compassion Network</h1>
-				<Link to="/">Home</Link>
-				<Switch>
-					<Route exact path="/" component={Dashboard} />
-				</Switch>
-			</div>
-		</Router>
+		<AuthState>
+			<Router>
+				<div>
+					<h1>Compassion Network</h1>
+					<Link to="/">Home</Link>
+					<Switch>
+						<Route exact path="/" component={Dashboard} />
+					</Switch>
+				</div>
+			</Router>
+		</AuthState>
 	);
 };
 
