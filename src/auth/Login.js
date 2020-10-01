65393eeb99afe9f129ae007db74445b8d56c4753
Stephen Peck <speck1990@gmail.com>
2020-09-30 22:47:58 -0500

    Setup component state for form values

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+const Login = props => {
+	const [user, setUser] = useState({
+		email: "",
+		password: ""
+	});
+
+	const { email, password } = user;
+
+	const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
 	return (
 		<div>
 			<form>
-				<input type="email" name="email" placeholder="Email" />
+				<input type="email" name="email" value={email} placeholder="Email" onChange={onChange} />
 				<br />
-				<input type="password" name="password" placeholder="Password" />
+				<input type="password" name="password" value={password} placeholder="Password" onChange={onChange} />
 				<br />
 				<button>Login</button>
 			</form>
