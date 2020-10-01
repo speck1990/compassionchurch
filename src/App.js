59cc2dc3bd671069200d4565f09dd3b55f71e1b8
Stephen Peck <speck1990@gmail.com>
2020-09-30 20:44:02 -0500

    Add Dashboard page to router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+import Dashboard from "./pages/Dashboard";
+
 const App = () => {
 	return (
 		<Router>
 			<div>
 				<h1>Compassion Network</h1>
 				<Link to="/">Home</Link>
-				<Switch></Switch>
+				<Switch>
+					<Route exact path="/" component={Dashboard} />
+				</Switch>
 			</div>
 		</Router>
 	);
