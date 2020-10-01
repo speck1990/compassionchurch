c49429aaf49d21b263c2dceec9dd071eaa71003a
Stephen Peck <speck1990@gmail.com>
2020-09-30 20:37:12 -0500

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
-	return <div>Compassion Church</div>;
+	return (
+		<Router>
+			<div>
+				<h1>Compassion Network</h1>
+				<Link to="/">Home</Link>
+				<Switch></Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
