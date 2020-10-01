3959921c032f0cd3a3910cb2f94b532bb1088b20
Stephen Peck <speck1990@gmail.com>
2020-09-30 23:18:04 -0500

    Create form submit function and useEffect

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,10 +1,17 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../context/auth/authContext";
 
 const Login = props => {
 	const authContext = useContext(AuthContext);
 
 	const { loginUser, isAuthenticated } = authContext;
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			props.history.push("/");
+		}
+		//eslint-disable-next-line
+	}, [isAuthenticated, props.history]);
 
 	const [user, setUser] = useState({
 		email: "",
@@ -15,9 +22,18 @@ const Login = props => {
 
 	const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
+	const onSubmit = e => {
+		e.preventDefault();
+		if (email === "" || password === "") {
+			console.log("Please complete the form.");
+		} else {
+			loginUser();
+		}
+	};
+
 	return (
 		<div>
-			<form>
+			<form onSubmit={onSubmit}>
 				<input type="email" name="email" value={email} placeholder="Email" onChange={onChange} />
 				<br />
 				<input type="password" name="password" value={password} placeholder="Password" onChange={onChange} />
