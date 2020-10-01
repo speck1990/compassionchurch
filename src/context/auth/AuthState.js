313bea8b8bb53c2d489f3960fd4a0a140ef37673
Stephen Peck <speck1990@gmail.com>
2020-09-30 22:10:23 -0500

    Create initial state and loadUser action

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -1,14 +1,45 @@
 import React, { useReducer } from "react";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
-import { LOGIN_SUCCESS } from "../types";
+import { USER_LOADED, LOGIN_SUCCESS } from "../types";
 
 const AuthState = props => {
-	const initalState = {};
+	const users = [
+		{
+			id: "1",
+			firstName: "Stephen",
+			lastName: "Peck",
+			email: "speck1990@gmail.com",
+			password: "password123"
+		}
+	];
+
+	const initalState = {
+		isAuthenticated: null,
+		user: null
+	};
 
 	const [state, dispatch] = useReducer(authReducer, initalState);
 
-	return <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>;
+	// Load User
+	const loadUser = async () => {
+		dispatch({
+			type: USER_LOADED,
+			payload: users[1]
+		});
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{
+				isAuthenticated: state.isAuthenticated,
+				user: state.user,
+				loadUser
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
 };
 
 export default AuthState;
--- src/context/auth/authReducer.js
+++ src/context/auth/authReducer.js
@@ -1,7 +1,15 @@
-import { LOGIN_SUCCESS } from "../types";
+import { USER_LOADED, LOGIN_SUCCESS } from "../types";
 
 const authReducer = (state, action) => {
 	switch (action.type) {
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: action.payload
+			};
+
 		case LOGIN_SUCCESS:
 		default:
 			return state;
