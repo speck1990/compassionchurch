4a0939ea435d8ff3507679a7d80dad85579e7832
Stephen Peck <speck1990@gmail.com>
2020-10-02 14:24:34 -0500

    Add pages for development

--- src/context/page/PageState.js
+++ src/context/page/PageState.js
@@ -4,7 +4,26 @@ import pageReducer from "./pageReducer";
 import {} from "../types";
 
 const PageState = props => {
-	const pages = [{}];
+	const pages = [
+		{
+			title: "About Us",
+			subtitle: "Get to know us",
+			content: [
+				{ type: "heading", text: "About Us" },
+				{ type: "paragraph", text: "Irure proident aliqua amet voluptate non non pariatur. Non nisi eu nulla consequat. Enim fugiat dolore quis dolor culpa. Ullamco reprehenderit proident nostrud voluptate deserunt ea elit qui reprehenderit enim duis deserunt adipisicing dolor." },
+				{ type: "button", text: "Click me", url: "http://www.google.com" }
+			]
+		},
+		{
+			title: "Contact Us",
+			subtitle: "Say hi to us",
+			content: [
+				{ type: "heading", text: "Cotact Us" },
+				{ type: "paragraph", text: "Irure proident aliqua amet voluptate non non pariatur. Non nisi eu nulla consequat. Enim fugiat dolore quis dolor culpa. Ullamco reprehenderit proident nostrud voluptate deserunt ea elit qui reprehenderit enim duis deserunt adipisicing dolor." },
+				{ type: "button", text: "Click me", url: "http://www.google.com" }
+			]
+		}
+	];
 
 	const initalState = {};
 
