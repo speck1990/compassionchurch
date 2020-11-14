3a253f333b8c8249cfa50d7c34b2e286a8b2cdb5
Stephen Peck <speck1990@gmail.com>
2020-11-14 12:28:22 -0600

    Delete old code

--- src/app/navigation/LinkForm.js
+++ src/app/navigation/LinkForm.js
@@ -129,52 +129,6 @@ const LinkForm = props => {
 			{/* container */}
 		</div>
 	);
-
-	// 	return (
-	// 		<div>
-	// 			{current !== null && !loading ? (
-	// 				<form onSubmit={handleOnClick}>
-	// 					<div>
-	// 						<label htmlFor="label">Link Label</label>
-	// 						<br />
-	// 						<input type="text" name="label" value={current.label} onChange={onTextChange} />
-	// 					</div>
-	// 					<div>
-	// 						<input type="radio" name="type" value="page" onChange={onLinkChange} checked={current.type === "page"} />
-	// 						<label htmlFor="page">Link to page</label>
-	// 					</div>
-	// 					<div>
-	// 						<input type="radio" name="type" value="external" onChange={onLinkChange} checked={current.type === "external"} />
-	// 						<label htmlFor="external">External Link</label>
-	// 					</div>
-	// 					<div>
-	// 						<label htmlFor="link">Link</label>
-	// 						{current.type === "page" ? (
-	// 							<select name="link" value={current.link} onChange={onTextChange}>
-	// 								<option></option>
-	// 								{pages.map((page, key) => (
-	// 									<option key={key} value={page.id}>
-	// 										{page.title}
-	// 									</option>
-	// 								))}
-	// 							</select>
-	// 						) : (
-	// 							<input type="input" name="link" value={current.link} onChange={onTextChange} />
-	// 						)}
-	// 					</div>
-	// 					<div>
-	// 						<input type="checkbox" id="newTab" name="newTab" onChange={e => onCheckboxChange(current.newTab, e)} defaultChecked={current.newTab} value={current.newTab} />
-	// 						<label htmlFor="newTab"> Open in new tab</label>
-	// 					</div>
-	// 					<br />
-
-	// 					<button onClick={handleOnClick}>Save & Publish</button>
-	// 				</form>
-	// 			) : (
-	// 				<p></p>
-	// 			)}
-	// 		</div>
-	// 	);
 };
 
 export default LinkForm;
