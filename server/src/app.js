cc6185a2394e2a2a91545d5d7952002279f31d20
Stephen Peck <speck1990@gmail.com>
2020-12-17 22:18:36 -0600

    Use cors and update api endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -2,20 +2,22 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 // Connect to db
 require("./db/mongoose");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/user", require("./routes/user"));
-app.use("/api/page", require("./routes/page"));
-app.use("/api/link", require("./routes/link"));
+app.use("/api/users", require("./routes/user"));
+app.use("/api/pages", require("./routes/page"));
+app.use("/api/links", require("./routes/link"));
 
 module.exports = app;
