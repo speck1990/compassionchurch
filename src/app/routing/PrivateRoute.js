b10af73dc51f9c624cd98dadd4b1a21940e42a7d
Stephen Peck <speck1990@gmail.com>
2020-12-17 22:27:44 -0600

    Add loadUser function so user is loaded when a private route is called

--- src/app/routing/PrivateRoute.js
+++ src/app/routing/PrivateRoute.js
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Route, Redirect } from "react-router-dom";
 import AuthContext from "../context/auth/authContext";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 	const authContext = useContext(AuthContext);
 
-	const { isAuthenticated, loading } = authContext;
+	const { loadUser, isAuthenticated, loading } = authContext;
+
+	useEffect(() => {
+		loadUser();
+		//eslint-disable-next-line
+	}, []);
 
 	return <Route {...rest} render={props => (!isAuthenticated && !loading ? <Redirect to="/login" /> : <Component {...props} />)} />;
 };
