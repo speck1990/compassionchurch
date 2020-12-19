6936c9e3e48500a072ec469ea4e19524ce8edf42
Stephen Peck <speck1990@gmail.com>
2020-12-19 11:45:20 -0600

    Add validation rules for when creating/updating pages

--- server/src/middleware/validation.js
+++ server/src/middleware/validation.js
@@ -4,7 +4,12 @@ const loginValidationRules = () => {
 	return [oneOf([check("email").notEmpty(), check("password").notEmpty()], "All fields are required."), check("email", "Please include a valid email").isEmail()];
 };
 
+const pageValidationRules = () => {
+	return [oneOf([check("title", "Title is required").not().isEmpty(), check("slug", "Slug is required").not().isEmpty()])];
+};
+
 module.exports = {
 	loginValidationRules,
+	pageValidationRules,
 	validationResult
 };
