2d30bcd97a5a67344ab729f7167897fb6de1a2c5
Stephen Peck <speck1990@gmail.com>
2020-12-29 22:25:46 -0600

    Change all id to _id

--- src/app/pageCreator/blocks/blocks/Paragraph.js
+++ src/app/pageCreator/blocks/blocks/Paragraph.js
@@ -15,13 +15,13 @@ const Paragraph = ({ block, index }) => {
 
 	const formats = ["bold", "italic", "underline", "indent", "link", "align"];
 
-	const handleOnChange = value => updateCurrent({ ...current, content: current.content.map(el => (el.id === block.id ? { id: block.id, type: "paragraph", text: value } : el)) });
+	const handleOnChange = value => updateCurrent({ ...current, content: current.content.map(el => (el._id === block._id ? { id: block._id, type: "paragraph", text: value } : el)) });
 
 	return (
-		<Draggable draggableId={`draggable-${block.id}`} index={index}>
+		<Draggable draggableId={`draggable-${block._id}`} index={index}>
 			{provided => (
 				<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="bg-white">
-					<button onClick={() => deleteBlock(block.id)}>Delete Below</button>
+					<button onClick={() => deleteBlock(block._id)}>Delete Below</button>
 					<ReactQuill modules={modules} formats={formats} value={block.text || ""} onChange={handleOnChange} />
 				</div>
 			)}
