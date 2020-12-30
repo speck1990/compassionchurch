0d5a0862839b834bc7f9e506b3e4126674ab759f
Stephen Peck <speck1990@gmail.com>
2020-12-29 22:28:44 -0600

    Add next callback function

--- server/src/middleware/validation.js
+++ server/src/middleware/validation.js
@@ -4,8 +4,9 @@ const loginValidationRules = () => {
 	return [oneOf([check("email").notEmpty(), check("password").notEmpty()], "All fields are required."), check("email", "Please include a valid email").isEmail()];
 };
 
-const pageValidationRules = () => {
-	return [oneOf([check("title", "Title is required").not().isEmpty(), check("slug", "Slug is required").not().isEmpty()])];
+const pageValidationRules = (req, res, next) => {
+	oneOf([check("title", "Title is required").not().isEmpty(), check("slug", "Slug is required").not().isEmpty()]);
+	next();
 };
 
 module.exports = {
