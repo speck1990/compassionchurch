442b6fd5c66d036e801def7bb83e1393b857399b
Stephen Peck <speck1990@gmail.com>
2021-01-09 16:16:01 -0600

    Add isSaved and getPages from context

--- src/app/pageCreator/PageForm.js
+++ src/app/pageCreator/PageForm.js
@@ -13,7 +13,7 @@ const PageForm = props => {
 	const { id } = useParams();
 	const pageContext = useContext(PageContext);
 
-	const { current, setCurrent, clearCurrent, updateCurrent, updatePage, addPage, loading, error } = pageContext;
+	const { current, setCurrent, clearCurrent, updateCurrent, updatePage, addPage, loading, error, isSaved, getPages } = pageContext;
 
 	const [isVisible, setIsVisible] = useState(true);
 
