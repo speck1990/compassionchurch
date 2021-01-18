4c87ee26867f416a768494b9cf3bcf5205096559
Stephen Peck <speck1990@gmail.com>
2021-01-18 11:51:21 -0600

    Create validation for page and link forms

--- server/src/middleware/validation.js
+++ server/src/middleware/validation.js
@@ -1,11 +1,77 @@
 const { check, oneOf, validationResult } = require("express-validator");
+const Page = require("../models/page");
+const Link = require("../models/link");
+
+/*
+
+VALIDATION FOR LOGIN FORM
+
+*/
 
 const loginValidationRules = () => {
 	return [oneOf([check("email").notEmpty(), check("password").notEmpty()], "All fields are required."), check("email", "Please include a valid email").isEmail()];
 };
 
+/*
+
+VALIDATION FOR PAGE FORM
+
+*/
+
+const validPage = () => {
+	return check("slug").custom((value, { req }) => {
+		// new page check if page title exists
+		// edit page, check if title has changed, if so check if title exists
+
+		if (req.params.id) {
+			return Page.findById(req.params.id).then(page => {
+				if (page.slug !== req.body.slug) {
+					return Page.findOne({ slug: value, user: req.user.id }).then(page => {
+						if (page) {
+							return Promise.reject("Slug already in use.");
+						}
+					});
+				}
+			});
+		}
+	});
+};
+
 const pageValidationRules = () => {
-	return [check("title", "Title is required").notEmpty(), check("slug", "Slug is required").notEmpty()];
+	return [
+		// Fields are required
+		check("title", "Title is required").notEmpty(),
+		check("slug", "Slug is required").notEmpty(),
+		// Page should be valid
+		validPage()
+	];
+};
+
+/*
+
+VALIDATION FOR LINK FORM
+
+*/
+
+const validLink = () => {
+	return check("label").custom((value, { req }) => {
+		return Link.findOne({ label: value, user: req.user.id }).then(link => {
+			if (link) {
+				return Promise.reject("Link already in use.");
+			}
+		});
+	});
+};
+
+const linkValidationRules = req => {
+	return [
+		// All fields are required
+		check("label", "Label is required").notEmpty(),
+		check("type", "Type is required").notEmpty(),
+		check("linkValue", "Link is required").notEmpty(),
+		// Link should be valid
+		validLink()
+	];
 };
 
 const validate = (req, res, next) => {
@@ -20,6 +86,7 @@ const validate = (req, res, next) => {
 module.exports = {
 	loginValidationRules,
 	pageValidationRules,
+	linkValidationRules,
 	validationResult,
 	validate
 };
