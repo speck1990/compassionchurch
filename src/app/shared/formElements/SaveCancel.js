9061f9d0ef66eff3e7dea8d751f6f3b206867291
Stephen Peck <speck1990@gmail.com>
2021-01-27 11:45:36 -0600

    Create boolean prop that gives the option to hide cancel button

--- src/app/shared/formElements/SaveCancel.js
+++ src/app/shared/formElements/SaveCancel.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const SaveCancel = ({ onSave, onCancel }) => {
+const SaveCancel = ({ onSave, onCancel, showCancel = true }) => {
 	const [showModal, setShowModal] = useState(false);
 
 	const closeModal = () => {
@@ -18,28 +18,33 @@ const SaveCancel = ({ onSave, onCancel }) => {
 			<button className="btn btn-az-primary pd-x-30 mg-r-5" onClick={e => handleSave(onSave, e)}>
 				Save & Publish
 			</button>
-			<Button className="btn btn-dark pd-x-30" onClick={() => setShowModal(true)}>
-				Cancel
-			</Button>
 
-			<Modal show={showModal} size="sm" onHide={() => closeModal("small")}>
-				<Modal.Header closeButton>
-					<Modal.Title>Cancel Changes?</Modal.Title>
-				</Modal.Header>
-
-				<Modal.Body>
-					<p>Are you sure you would like to cancel all changes?</p>
-				</Modal.Body>
-
-				<Modal.Footer>
-					<Button variant="danger" onClick={onCancel}>
-						Yes
+			{showCancel && (
+				<Fragment>
+					<Button className="btn btn-dark pd-x-30" onClick={() => setShowModal(true)}>
+						Cancel
 					</Button>
-					<Button variant="outline-light" onClick={closeModal}>
-						No
-					</Button>
-				</Modal.Footer>
-			</Modal>
+
+					<Modal show={showModal} size="sm" onHide={() => closeModal("small")}>
+						<Modal.Header closeButton>
+							<Modal.Title>Cancel Changes?</Modal.Title>
+						</Modal.Header>
+
+						<Modal.Body>
+							<p>Are you sure you would like to cancel all changes?</p>
+						</Modal.Body>
+
+						<Modal.Footer>
+							<Button variant="danger" onClick={onCancel}>
+								Yes
+							</Button>
+							<Button variant="outline-light" onClick={closeModal}>
+								No
+							</Button>
+						</Modal.Footer>
+					</Modal>
+				</Fragment>
+			)}
 		</div>
 	);
 };
