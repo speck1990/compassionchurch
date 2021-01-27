07fc60050c92cbd6f0cabc2b8c330182b75e5c60
Stephen Peck <speck1990@gmail.com>
2021-01-27 16:35:53 -0600

    Move function to get campus to helper file

--- website/src/routes/index.js
+++ website/src/routes/index.js
@@ -1,34 +1,10 @@
 var express = require("express");
 var router = express.Router();
-
-const campuses = [
-	{
-		name: "Compassion Church of Oklahoma City",
-		subdomain: "okc",
-		domain: "compassionokc.church",
-		city: "Oklahoma City",
-		state: "Oklahoma"
-	},
-	{
-		name: "Compassion Church of Wichita Falls",
-		subdomain: "wichitafalls",
-		domain: "compassionwf.church",
-		city: "Wichita Falls",
-		state: "Texas"
-	}
-];
-
-function getSubdomain(h) {
-	var parts = h.split(".");
-	if (parts.length === 2) return "www";
-	return parts[0];
-}
+const getCampus = require("../helpers/getCampus");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	const subdomain = getSubdomain(req.headers.host);
-
-	const campus = campuses.find(campus => campus.subdomain === subdomain);
+	const campus = getCampus(req.headers.host);
 
 	res.render("index", { campus });
 });
