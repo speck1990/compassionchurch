5f51dd4bd07d76efe169e738fb66ba713d8501dd
Stephen Peck <speck1990@gmail.com>
2021-02-06 12:20:23 -0600

    Add styles to block options

--- src/app/pageCreator/blocks/BlockOptions.js
+++ src/app/pageCreator/blocks/BlockOptions.js
@@ -12,27 +12,29 @@ const BlockOptions = () => {
 		<Droppable droppableId="options" isDropDisabled={true}>
 			{(provided, snapshot) => (
 				<div ref={provided.innerRef}>
-					{blockTypes.map((type, key) => (
-						<Draggable key={key} draggableId={`draggable-${type}`} index={key}>
-							{(provided, snapshot) => (
-								<Fragment>
-									<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} style={provided.draggableProps.style} className="mg-b-10">
-										<Button variant="primary btn-rounded mg-l-5 mg-r-5" key={key}>
-											{type}
-										</Button>
-									</div>
-									{snapshot.isDragging && (
-										<div className="dragging-static-copy mg-b-10">
-											<Button variant="primary btn-rounded mg-l-5 mg-r-5" key={key}>
-												{type}
-											</Button>
-										</div>
+					<div className="az-icon-group bg-gray-200">
+						<div className="row row-xs">
+							{blockTypes.map((block, key) => (
+								<Draggable key={key} draggableId={`draggable-${block.type}`} index={key}>
+									{(provided, snapshot) => (
+										<Fragment>
+											<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} style={provided.draggableProps.style} className="col">
+												<i class={block.icon}></i>
+												<div>{block.type}</div>
+											</div>
+											{snapshot.isDragging && (
+												<div className="dragging-static-copy col">
+													<i class={block.icon}></i>
+													<div>{block.type}</div>
+												</div>
+											)}
+										</Fragment>
 									)}
-								</Fragment>
-							)}
-						</Draggable>
-					))}
-					{provided.placeholder}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</div>
+					</div>
 				</div>
 			)}
 		</Droppable>
