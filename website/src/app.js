d51f88d2156318d836130fdb91f48eaa18a98e47
Stephen Peck <speck1990@gmail.com>
2021-02-09 23:14:58 -0600

    Change the public folder location

--- website/src/app.js
+++ website/src/app.js
@@ -14,12 +14,13 @@ require("./helpers/hbs");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.set("view options", { layout: "layouts/main" });
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
