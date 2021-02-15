12597ffc0b0a57400513106e5cde85972a8a0cc2
Stephen Peck <speck1990@gmail.com>
2021-02-15 12:32:08 -0600

    Change url to include /public for public folder

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.use("/api/users", require("./routes/user"));
 app.use("/api/locations", require("./routes/location"));
