0584437a9a56d4f53b7a6cd9e0a42782de3d22d0
Stephen Peck <speck1990@gmail.com>
2021-02-15 17:05:55 -0600

    Add a ifEquals handlebars helper

--- website/src/helpers/hbs.js
+++ website/src/helpers/hbs.js
@@ -6,7 +6,11 @@ hbs.registerHelper("switch", function (value, options) {
 });
 
 hbs.registerHelper("case", function (value, options) {
-	if (value == this.switch_value) {
+	if (value === this.switch_value) {
 		return options.fn(this);
 	}
+});
+
+hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
+	return arg1 === arg2 ? options.fn(this) : options.inverse(this);
 });
