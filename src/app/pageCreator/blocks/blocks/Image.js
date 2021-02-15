01b9750bbd47e342d74b735f55e66ebaef47d52d
Stephen Peck <speck1990@gmail.com>
2021-02-15 12:41:24 -0600

    Use Dropzone component

--- src/app/pageCreator/blocks/blocks/Image.js
+++ src/app/pageCreator/blocks/blocks/Image.js
@@ -1,63 +1,15 @@
-import React, { Fragment, useMemo, useState } from "react";
-import { useDropzone } from "react-dropzone";
+import React, { useContext } from "react";
+import Dropzone from "../../../shared/formElements/Dropzone";
+import PageContext from "../../../context/page/pageContext";
 
-const baseStyle = {
-	textAlign: "center",
-	padding: "20px",
-	borderWidth: 2,
-	borderRadius: 2,
-	borderColor: "#eeeeee",
-	borderStyle: "dashed",
-	backgroundColor: "#fafafa",
-	color: "#bdbdbd",
-	outline: "none",
-	transition: "border .24s ease-in-out"
-};
+const Image = ({ block, index, label, deleteButton = false }) => {
+	const pageContext = useContext(PageContext);
 
-const activeStyle = {
-	borderColor: "#2196f3"
-};
+	const { current, updateCurrent } = pageContext;
 
-const acceptStyle = {
-	borderColor: "#00e676"
-};
+	const onImageDrop = url => updateCurrent({ ...current, content: current.content.map(el => (el._id === block._id ? { _id: block._id, type: "image", url } : el)) });
 
-const rejectStyle = {
-	borderColor: "#ff1744"
-};
-
-const Image = props => {
-	const [file, setFile] = useState([]);
-	const onDrop = (files, rejectedFiles) => {
-		setFile(URL.createObjectURL(files[0]));
-	};
-
-	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({ onDrop, accept: "image/*", multiple: false });
-
-	const style = useMemo(
-		() => ({
-			...baseStyle,
-			...(isDragActive ? activeStyle : {}),
-			...(isDragAccept ? acceptStyle : {}),
-			...(isDragReject ? rejectStyle : {})
-		}),
-		[isDragActive, isDragReject, isDragAccept]
-	);
-
-	return (
-		<Fragment>
-			{file.length === 0 ? (
-				<div {...getRootProps({ style })}>
-					<input {...getInputProps()} />
-					<p>Drag 'n' drop an image here, or click to select an image</p>
-				</div>
-			) : (
-				<div>
-					<img alt="test" style={{ width: "100px", height: "auto" }} src={file} />
-				</div>
-			)}
-		</Fragment>
-	);
+	return <Dropzone onDrop={onImageDrop} image={block.url} />;
 };
 
 export default Image;
