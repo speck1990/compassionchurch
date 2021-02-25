d645bfad69d331ad6c2a6721ff39bc3f317a2f16
Stephen Peck <speck1990@gmail.com>
2021-02-24 22:53:34 -0600

    Create validator function that returns errors if any

---
+++ src/app/validation/validator.js
@@ -0,0 +1,10 @@
+export const validator = (values, schema) => {
+	return schema
+		.validate(values, { abortEarly: false })
+		.then(() => null)
+		.catch(error => {
+			let errors = {};
+			error.inner.forEach(err => (errors[err.path] = err.errors[0]));
+			return { ...errors };
+		});
+};
