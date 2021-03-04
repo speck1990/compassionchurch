42d61a095f3d2c4c28dc05d36f557d16b725b30f
Stephen Peck <speck1990@gmail.com>
2021-03-04 16:19:35 -0600

    Convert validator function to return a promise

--- src/app/validation/validator.js
+++ src/app/validation/validator.js
@@ -1,10 +1,14 @@
 export const validator = (values, schema) => {
-	return schema
-		.validate(values, { abortEarly: false })
-		.then(() => "")
-		.catch(error => {
-			let errors = {};
-			error.inner.forEach(err => (errors[err.path] = err.errors[0]));
-			return { ...errors };
-		});
+	let promise = new Promise(function (resolve, reject) {
+		schema
+			.validate(values, { abortEarly: false })
+			.then(() => resolve(""))
+			.catch(error => {
+				let errors = {};
+				error.inner.forEach(err => (errors[err.path] = err.errors[0]));
+				resolve({ ...errors });
+			});
+	});
+
+	return promise;
 };
