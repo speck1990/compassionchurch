bcaa021fec1bd3397f2d92ce8459177ee8c9eb76
Stephen Peck <speck1990@gmail.com>
2021-03-18 22:11:37 -0500

    Update onImageDrop to work for Sections

--- src/app/pageCreator/blocks/blocks/Image.js
+++ src/app/pageCreator/blocks/blocks/Image.js
@@ -2,12 +2,20 @@ import React, { useContext } from "react";
 import Dropzone from "../../../shared/formElements/Dropzone";
 import PageContext from "../../../context/page/pageContext";
 
-const Image = ({ block, index, label, deleteButton = false }) => {
+const Image = ({ block, parent, index, label, deleteButton = false }) => {
 	const pageContext = useContext(PageContext);
 
 	const { current, updateCurrent } = pageContext;
 
-	const onImageDrop = url => updateCurrent({ ...current, content: current.content.map(el => (el._id === block._id ? { _id: block._id, type: "image", url } : el)) });
+	const onImageDrop = url => {
+		const updatedBlock = { _id: block._id, type: "image", url: url };
+
+		const parentSection = current.content.find(section => section._id === parent._id);
+		const newBlocks = parentSection.content.map(bl => (bl._id === block._id ? updatedBlock : bl));
+		parentSection.content = newBlocks;
+
+		updateCurrent({ ...current, content: current.content.map(section => (section._id === parent._id ? parentSection : section)) });
+	};
 
 	return <Dropzone onDrop={onImageDrop} image={block.url} />;
 };
