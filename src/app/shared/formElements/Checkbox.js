57e3f83f7863a138f641ce5a52999c786d564d47
Stephen Peck <speck1990@gmail.com>
2021-03-30 19:14:44 -0500

    Add functionality so checkbox is one column

--- src/app/shared/formElements/Checkbox.js
+++ src/app/shared/formElements/Checkbox.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Form } from "react-bootstrap";
 
 const Checkbox = props => {
 	return (
-		<div className="row row-xs align-items-center mg-b-15">
-			<div className="col-md-4"></div>
-			<div className="col-md-5 mg-t-5 mg-md-t-0">
-				<Form.Check type="checkbox" name={props.name} id={props.name} label={props.label} defaultChecked={props.value} className={props.error ? "is-invalid ckbox" : "chkbox"} onChange={e => props.onCheckboxChange(props.value, e)} />
-				{props.error ? (
-					<Form.Control.Feedback type="invalid">{props.error}</Form.Control.Feedback>
-				) : (
-					<Form.Text className="text-muted" style={!props.text ? { marginBottom: "20px" } : {}}>
-						{props.text}
-					</Form.Text>
-				)}
-			</div>
-		</div>
+		<Fragment>
+			{props.small ? (
+				<div>
+					<Form.Check type="checkbox" name={props.name} id={props.name} label={props.label} defaultChecked={props.value} className={props.error ? "is-invalid ckbox" : "chkbox"} onChange={e => props.onCheckboxChange(props.value, e)} />
+					{props.error ? (
+						<Form.Control.Feedback type="invalid">{props.error}</Form.Control.Feedback>
+					) : (
+						<Form.Text className="text-muted" style={!props.text ? { marginBottom: "20px" } : {}}>
+							{props.text}
+						</Form.Text>
+					)}
+				</div>
+			) : (
+				<div className="row row-xs align-items-center mg-b-15">
+					<div className="col-md-4"></div>
+					<div className="col-md-8 mg-t-5 mg-md-t-0">
+						<Form.Check type="checkbox" name={props.name} id={props.name} label={props.label} defaultChecked={props.value} className={props.error ? "is-invalid ckbox" : "chkbox"} onChange={e => props.onCheckboxChange(props.value, e)} />
+						{props.error ? (
+							<Form.Control.Feedback type="invalid">{props.error}</Form.Control.Feedback>
+						) : (
+							<Form.Text className="text-muted" style={!props.text ? { marginBottom: "20px" } : {}}>
+								{props.text}
+							</Form.Text>
+						)}
+					</div>
+				</div>
+			)}
+		</Fragment>
 	);
 };
 
