d04ed038e3ab8bb7d4ed7d36d40db004450690b0
Stephen Peck <speck1990@gmail.com>
2021-04-14 22:11:02 -0500

    Add additional endpoints for locations

--- server/src/routes/location.js
+++ server/src/routes/location.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const Location = require("../models/location");
 const Page = require("../models/page");
@@ -42,14 +43,65 @@ router.get("/:location", async (req, res) => {
 // @route       POST api/location
 // @desc        Create a location
 // @access      Private
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
 	try {
-		const location = new Location({ name: "Compassion Church OKC", subdomain: "okc", domain: "compassionokc.church", address: "123 S Road", city: "Oklahoma City", state: "Oklahoma", zip: "12345" });
+		const location = new Location({ ...req.body });
+		// {name: "Compassion Church OKC", subdomain: "okc", domain: "compassionokc.church", address: "123 S Road", city: "Oklahoma City", state: "Oklahoma", zip: "12345"}
 		await location.save();
 		res.json(location);
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send({ msg: err.message });
+	}
+});
+
+// @route       PUT api/location/:id
+// @desc        Update a location
+// @access      Private
+router.put("/:id", auth, async (req, res) => {
+	const id = req.params.id;
+
+	try {
+		let location = await Location.findById(id);
+
+		if (!location) return res.status(404).json({ msg: "Page not found" });
+
+		// Make sure user is admin
+		// if (!req.user.admin) {
+		// 	return res.status(401).json({ msg: "Not authorized" });
+		// }
+
+		location = await Location.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+
+		res.json(location);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({ msg: err.message });
+	}
+});
+
+// @route       DELETE api/locations/:id
+// @desc        Delete a location
+// @access      Private
+router.delete("/:id", auth, async (req, res) => {
+	const id = req.params.id;
+
+	try {
+		let location = await Location.findById(id);
+
+		if (!location) return res.status(404).json({ msg: "Location not found" });
+		console.log(req.user);
+		// Make sure user is admin
+		// if (!req.user.admin) {
+		// 	return res.status(401).json({ msg: "Not authorized" });
+		// }
+
+		await Location.findByIdAndRemove(id);
+
+		res.json({ msg: "Location deleted" });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
 	}
 });
 
